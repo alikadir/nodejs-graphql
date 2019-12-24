0f87de69dd7e2489c647a2ad58e3bf9b386e8f21
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2019-12-24 17:01:12 +0300

    enum

--- pure-server.js
+++ pure-server.js
@@ -4,32 +4,40 @@ import graphqlHTTP from 'express-graphql';
 import externalSchema from './schema/basic-schema';
 var app = express();
 
-//#region Build Schema
+//#region Build Schema with SDL (Schema Definition Language)
 import postJson from './data/posts.json';
 
 import { buildSchema } from 'graphql';
 var schema = buildSchema(`	
-  type post {	
+enum Status {
+    new
+    updated
+    deleted
+  } 
+type Post {	
     id: Int
     userId: Int!	
     """
     title is describe to post gql
     """
     title: String	
-    body: String	
+    body: String
+    status: Status    
   }	
-  type Query {
-    posts: [post]
-    singlePost(id: Int): post
-    userPosts(userId: Int): [post]
+type Query {
+    posts: [Post]
+    singlePost(id: Int): Post
+    userPosts(userId: Int): [Post]
+    getPostsByStatus(status: Status): [Post]
   }
 `);
 var root = {
   posts: () => postJson,
   singlePost: args => postJson.find(x => x.id == args.id),
-  userPosts: args => postJson.filter(x => x.userId == args.userId)
+  userPosts: args => postJson.filter(x => x.userId == args.userId),
+  getPostsByStatus: args => postJson.filter(x => x.status == args.status)
 };
-app.use('/graphql-gql', graphqlHTTP({ schema: schema, rootValue: root, graphiql: true }));
+app.use('/graphql-sdl', graphqlHTTP({ schema: schema, rootValue: root, graphiql: true }));
 //#endregion
 
 app.use('/graphql-js', graphqlHTTP({ schema: externalSchema, graphiql: true }));
--- schema/basic-schema.js
+++ schema/basic-schema.js
@@ -1,5 +1,22 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList, GraphQLNonNull } from 'graphql';
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType
+} from 'graphql';
 import postJson from '../data/posts.json';
+
+const status = new GraphQLEnumType({
+  name: 'Status',
+  values: {
+    New: { value: 'new' },
+    Updated: { value: 'updated' },
+    Deleted: { value: 'deleted' }
+  }
+});
 
 const post = new GraphQLObjectType({
   name: 'Post',
@@ -7,7 +24,8 @@ const post = new GraphQLObjectType({
     id: { type: GraphQLInt },
     userId: { type: new GraphQLNonNull(GraphQLInt) },
     title: { type: GraphQLString, description: 'title is describe to post js' },
-    body: { type: GraphQLString }
+    body: { type: GraphQLString },
+    status: { type: status }
   }
 });
 
@@ -33,6 +51,15 @@ export default new GraphQLSchema({
         args: { userId: { type: GraphQLInt } },
         resolve(parent, args) {
           return postJson.filter(x => x.userId == args.userId);
+        }
+      },
+      getPostsByStatus: {
+        type: GraphQLList(post),
+        args: {
+          status: { type: status }
+        },
+        resolve(parent, args) {
+          return postJson.filter(x => x.status == args.status);
         }
       }
     }
