7eb92b955a29b41169db765f7fae4b68973dffd3
Ali Kadir BAGCIOGLU <alikadirbagcioglu@gmail.com>
2019-12-25 16:58:42 +0300

    apollo query update

--- apollo-server.js
+++ apollo-server.js
@@ -1,27 +1,51 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
+import postJson from './data/posts.json';
+
 // String, Int, Float, Boolean, [], ID = String identity
 // String! is a non-nullable string.
 
 var typeDefs = gql`
-  type Query {
-    name: String
+  enum Status {
+    new
+    updated
+    deleted
+  }
+  type Post {
     id: Int
+    userId: Int!
     """
-    age field is describe that bla bla
+    title is describe the post
     """
-    age: Int
-    isMale: Boolean
+    title: String
+    body: String
+    status: Status
+  }
+  type Query {
+    posts: [Post]
+    singlePost(id: Int): Post
+    userPosts(userId: Int): [Post]
+    getPostsByStatus(status: Status): [Post]
+  }
+  type Mutation {
+    sum(num1: Int, num2: Int): Float
   }
 `;
+// mutation is delete, update, create
+// query is only select, that even to be a function
+
+// mutation has two parameter, first parameter (maybe) parent second parameter args
 
 var resolvers = {
   Query: {
-    name: () => 'Ali',
-    id: () => 2,
-    age: () => 34,
-    isMale: () => true
+    posts: () => postJson,
+    singlePost: args => postJson.find(x => x.id == args.id),
+    userPosts: args => postJson.filter(x => x.userId == args.userId),
+    getPostsByStatus: args => postJson.filter(x => x.status == args.status)
+  },
+  Mutation: {
+    sum: (parent, args) => args.num1 / args.num2
   }
 };
 
